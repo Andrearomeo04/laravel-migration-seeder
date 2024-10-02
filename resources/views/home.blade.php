03d4c079bdd345ba6e69595b85756ae779243845
andrearomeo04 <pisco004@gmail.com>
2024-10-02 23:57:26 +0200

    [UPD] created cards for visualizing all information for train

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,24 @@
         </div>
     </div>
 </div>
+<div class="container">
+    <div class="row g-5">
+        @foreach ($trains as $train)
+        <div class="col-4 border border-black rounded">
+            <h2 class="text-center">{{ $train->company }}</h2>
+            <ul class="list-unstyled">
+                <li>{{ $train->departure_station }}</li>
+                <li><hr>{{ $train->arrival_station }}</li>
+                <li><hr>{{ $train->departure_time }}</li>
+                <li><hr>{{ $train->arrival_time }}</li>
+                <li><hr>{{ $train->train_code }}</li>
+                <li><hr>{{ $train->n_carriages }}</li>
+                <li><hr>{{ $train->on_time }}</li>
+                <li><hr>{{ $train->cancelled }}</li>
+            </ul>
+            
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
