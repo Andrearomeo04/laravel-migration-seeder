222bd792a5e6a0983a06406ee3eca78d4e79b3fd
andrearomeo04 <pisco004@gmail.com>
2024-10-03 00:02:44 +0200

    [FIX] fixato la mostra delle info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,16 @@
     <div class="row g-5">
         @foreach ($trains as $train)
         <div class="col-4 border border-black rounded">
-            <h2 class="text-center">{{ $train->company }}</h2>
+            <h2 class="text-center">Compagnia:{{ $train->company }}</h2>
             <ul class="list-unstyled">
-                <li>{{ $train->departure_station }}</li>
-                <li><hr>{{ $train->arrival_station }}</li>
-                <li><hr>{{ $train->departure_time }}</li>
-                <li><hr>{{ $train->arrival_time }}</li>
-                <li><hr>{{ $train->train_code }}</li>
-                <li><hr>{{ $train->n_carriages }}</li>
-                <li><hr>{{ $train->on_time }}</li>
-                <li><hr>{{ $train->cancelled }}</li>
+                <li>Stazione di partenza: {{ $train->departure_station }}</li>
+                <li><hr>Stazione di arrivo: {{ $train->arrival_station }}</li>
+                <li><hr>Orario di partenza: {{ $train->departure_time }}</li>
+                <li><hr>Orario di arrivo: {{ $train->arrival_time }}</li>
+                <li><hr>Codice Treno: {{ $train->train_code }}</li>
+                <li><hr>Numero Carrozze: {{ $train->n_carriages }}</li>
+                <li><hr>in orario: {{ $train->on_time? 'si!' : 'no' }}</li>
+                <li><hr>Cancellato: {{ $train->cancelled? 'si' : 'no' }}</li>
             </ul>
             
         </div>
